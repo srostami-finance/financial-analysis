62e3d2237140b8017a30ce3aff55d6d6672dd24f
sara-ri <sararostamiii1997@gmail.com>
2026-01-25 12:26:23 +0330

    Stage 3: Add Quick Ratio calculation

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import pandas as pd
 
 # ----------- Stage 1: Load Data -----------
 def load_data(file_path):
-    df = pd.read_excel(file_path)
-    return df
+    try:
+        df = pd.read_excel(file_path)
+        return df
+    except FileNotFoundError:
+        raise FileNotFoundError(f"{file_path} not found.")
 
 
 # ----------- Stage 2: Current Ratio -----------
@@ -13,20 +16,30 @@ def current_ratio(df):
     return df
 
 
-# ----------- Main Function -----------
+# ----------- Stage 3: Quick Ratio -----------
+def quick_ratio(df):
+    df['Quick_Ratio'] = (df['CurrentAssets'] - df['Inventory']) / df['CurrentLiabilities']
+    return df
+
+
+# ---------- Main Function ----------
 def main():
-    # مسیر فایل ورودی
     input_file = "../data/raw/financial_data.xlsx"
 
-    # بارگذاری داده
+    # Stage 1: Load Data
     df = load_data(input_file)
     print("Initial data loaded:")
     print(df.head())
 
-    # محاسبه Current Ratio
+    # Stage 2: Current Ratio
     df = current_ratio(df)
     print("\nAfter Current Ratio calculation:")
     print(df[['CurrentAssets', 'CurrentLiabilities', 'Current_Ratio']].head())
+
+    # Stage 3: Quick Ratio
+    df = quick_ratio(df)
+    print("\nAfter Quick Ratio calculation:")
+    print(df[['CurrentAssets', 'Inventory', 'CurrentLiabilities', 'Quick_Ratio']].head())
 
 
 if __name__ == "__main__":
